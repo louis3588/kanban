9177be43806fabe4bea0fd6efaaaf002a7969550
c21079342 <pollyl@cardiff.ac.uk>
2026-09-11 13:19:00 +0100

    created auth pages

---
+++ kanbanFrontend/src/app/auth/Register.tsx
@@ -0,0 +1,11 @@
+import {View} from "react-native";
+
+export default function RegistrationScreen() {
+    return (
+        <View>
+            <Text>
+                Register
+            </Text>
+        </View>
+    )
+}
---
+++ kanbanFrontend/src/app/auth/login.tsx
@@ -0,0 +1,11 @@
+import {View} from "react-native";
+
+export default function LoginScreen(){
+    return (
+        <View>
+            <Text>
+                Login
+            </Text>
+        </View>
+    )
+}
